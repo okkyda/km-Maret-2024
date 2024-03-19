d7b22f0caab3d8f9d942ea4ee900eaa311598b76
okkyda <odioananda18@gmail.com>
2024-03-19 21:31:06 +0700

    Isi Latihan JS

---
+++ Latihan.js
@@ -0,0 +1,36 @@
+// //Kubus 
+
+// var p = 2;
+// var l = 4;  
+// var kubus = 2*p + 2*l;
+// console.log(kubus);
+
+// // Luas Permukaan Tabun 
+
+// var pi = 3.14;
+// var r = 2; 
+// var t = 3; 
+// var tabung = 2*pi*r*r + 2*pi*r*t; 
+// console.log(tabung);
+
+// //1/4 Volume Bola 
+
+// var bola = 4/3*pi*r*r*r;
+// console.log(bola);
+
+// // Variabel 
+
+// var num = 10;
+// console.log(num);
+
+// var str = "buku";
+// console.log(str);
+
+// var isCorrect = true;
+// console.log(isCorrect);
+
+var a = 10;
+var b = 'b';
+var total = a - b;
+console.log(total);
+
